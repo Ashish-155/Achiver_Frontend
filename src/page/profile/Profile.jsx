9ea649830520fffd293c1ec62b7d0f07eb20169c
Gouranga Ghosh <gouranga.econstra@gmail.com>
2024-08-23 10:26:45 +0530

    profile page added

--- src/page/profile/Profile.jsx
+++ src/page/profile/Profile.jsx
@@ -1,7 +1,8 @@
-import { React, useContext, useEffect } from 'react'
+import { React, useContext, useEffect, useState } from 'react'
 import { LoginContext } from '../../ContextProvider/Context';
 import axios from 'axios';
 import { BASE_URL } from '../../services/api';
+import { Modal } from 'react-bootstrap';
 
 const Profile = () => {
     const { logindata, setLoginData } = useContext(LoginContext);
@@ -27,21 +28,31 @@ const Profile = () => {
         profile();
     }, []);
 
+
+    const [modalShow, setModalShow] = useState(false);
+
+    const openModalShow = () => setModalShow(true);
+    const closeModalShow = () => setModalShow(false);
+
     return (
-        <div className='dashboard profile_info'>
-            <div className=' overflow-x-hidden overflow-y-hidden'>
-                <div className='container'>
-                    <div className='main_content'>
-                        <div className="card-box">
-                            <button className='logout'><i class="fi fi-br-power"></i></button>
-                            <form>
-                                <label for="fileToUpload" className='upload'>
+        <>
+
+            <div className='dashboard profile_info'>
+                <div className=' overflow-x-hidden overflow-y-hidden'>
+                    <div className='container'>
+                        <div className='main_content'>
+
+                            <div className="profile-card">
+                                <div className="header">
+
+                                </div>
+                                <div className='profile_body'>
+
                                     <div className="exp-avtar gth-bg-danger text-white profile-pic">
 
-                                        {/* <img src="https://bootdey.com/img/Content/avatar/avatar7.png" className="rounded-circle avatar-xl img-thumbnail" alt="profile-image" /> */}
                                         {logindata.profile_image ? (
                                             <img
-                                                className="rounded-circle avatar-xl img-thumbnail"
+                                                className="rounded-circle avatar_img"
                                                 src={`${BASE_URL}/uploads/${logindata.profile_image}`}
                                                 alt={`${logindata.name}'s Profile`}
                                             />
@@ -49,35 +60,82 @@ const Profile = () => {
                                             <p>No profile image available</p>
                                         )}
 
-                                        <span className="glyphicon ">
+                                        <button className="glyphicon " onClick={openModalShow}>
                                             <i className="fi fi-sr-camera-viewfinder"></i>
-                                        </span>
+                                        </button>
                                     </div>
-                                </label>
-                                <input type="File" name="fileToUpload" id="fileToUpload" />
-                            </form>
 
 
-                            <div className="text-left mt-4">
+                                    <h2 className='heading2'>Samantha Jones</h2>
+                                    <p className="text-muted mb-2 font-13 ">{logindata.email}</p>
+                                    <div className="profileInfo">
+                                        <p className="text-muted mb-2 font-13 d-flex justify-content-between align-items-center"><strong>Full Name :</strong> <span className="ml-2">{logindata.name}</span></p>
 
-                                <p className="text-muted mb-2 font-13"><strong>Full Name :</strong> <span className="ml-2">{logindata.name}</span></p>
+                                        <p className="text-muted mb-2 font-13 d-flex justify-content-between align-items-center"><strong>Mobile :</strong><span className="ml-2">{logindata.contact_no}</span></p>
 
-                                <p className="text-muted mb-2 font-13"><strong>Mobile :</strong><span className="ml-2">{logindata.contact_no}</span></p>
+                                        <p className="text-muted mb-2 font-13 d-flex justify-content-between align-items-center"><strong>Email :</strong> <span className="ml-2 ">{logindata.email}</span></p>
+                                        <p className="text-muted mb-2 font-13 d-flex justify-content-between align-items-center"><strong>ISD_Code :</strong> <span className="ml-2 ">{logindata.isd_code}</span></p>
+                                        <p className="text-muted mb-2 font-13 d-flex justify-content-between align-items-center"><strong>Whats_App :</strong> <span className="ml-2 ">{logindata.whats_app_number}</span></p>
 
-                                <p className="text-muted mb-2 font-13"><strong>Email :</strong> <span className="ml-2 ">{logindata.email}</span></p>
-                                <p className="text-muted mb-2 font-13"><strong>ISD_Code :</strong> <span className="ml-2 ">{logindata.isd_code}</span></p>
-                                <p className="text-muted mb-2 font-13"><strong>Whats_App :</strong> <span className="ml-2 ">{logindata.whats_app_number}</span></p>
+                                        <p className="text-muted mb-1 font-13 d-flex justify-content-between align-items-center"><strong>Location :</strong> <span className="ml-2">{logindata.location}</span></p>
+                                        <p className="text-muted mb-1 font-13 d-flex justify-content-between align-items-center"><strong>Date Of Birth :</strong> <span className="ml-2">{logindata.date_of_birth}</span></p>
 
-                                <p className="text-muted mb-1 font-13"><strong>Location :</strong> <span className="ml-2">{logindata.location}</span></p>
-                                <p className="text-muted mb-1 font-13"><strong>Date Of Birth :</strong> <span className="ml-2">{logindata.date_of_birth}</span></p>
+                                    </div>
+                                </div>
                             </div>
-
-
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+
+
+
+
+
+
+
+            <Modal
+                show={modalShow}
+                onHide={closeModalShow}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Body className='p-0'>
+                    <div className="welcome_container">
+                        <button className='closeButton' onClick={closeModalShow}>
+                            <i className="fi fi-rr-circle-xmark"></i>
+                        </button>
+                        <div className="modal-body">
+                            <p className="modal-title">Upload a file</p>
+                            <p className="modal-description">Attach the file below</p>
+                            <label for="file" className="upload-area">
+                                <span className="upload-area-icon">
+
+                                    <i class="fi fi-rr-file-export fs-2 textGreen"></i>
+                                </span>
+                                <span className="upload-area-title"> file(s) here to upload.</span>
+                                <span className="upload-area-description">
+                                    Alternatively, you can select a file
+                                </span>
+                            </label>
+                            <input type='file' className='img_upload' id='file' />
+                        </div>
+                        <div className="modal-footer">
+                        <label for="tb-file-upload" className='primaryBtn'>Upload Image</label>
+                        <input type="file" id="tb-file-upload" />
+                            {/* <label for="file" >
+                                <button className="primaryBtn">
+                                   Upload File</button>
+                            </label>
+                            <input type='file' className='img_upload' id='file' /> */}
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+
     )
 }
 
